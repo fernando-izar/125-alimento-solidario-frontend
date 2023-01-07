49613955630f50d3b2eb176d32eabd54d7e98e11
fernando-izar <fizar@me.com>
2023-01-07 15:47:22 -0300

    feat: localhostDjango wip

--- src/services/api.ts
+++ src/services/api.ts
@@ -3,7 +3,9 @@ import axios from "axios";
 const api = axios.create({
   // baseURL: "https://api-m3-g2.herokuapp.com/",
   // baseURL: "http://localhost:3000/",
-  baseURL: "https://api-m4-g19.herokuapp.com/",
+  baseURL: "http://localhost:8000/api/",
+  // baseURL: "http://ec2-54-233-120-17.sa-east-1.compute.amazonaws.com:8000/api/",
+
   timeout: 5000,
 });
 
