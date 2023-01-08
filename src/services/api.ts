726da5459f9399e1429cc73e69a1b182d681c6eb
fernando-izar <fizar@me.com>
2023-01-08 07:18:21 -0300

    feat: AWS connection OK

--- src/services/api.ts
+++ src/services/api.ts
@@ -3,8 +3,8 @@ import axios from "axios";
 const api = axios.create({
   // baseURL: "https://api-m3-g2.herokuapp.com/",
   // baseURL: "http://localhost:3000/",
-  baseURL: "http://localhost:8000/api/",
-  // baseURL: "http://ec2-54-233-120-17.sa-east-1.compute.amazonaws.com:8000/api/",
+  // baseURL: "http://localhost:8000/api/",
+  baseURL: "http://ec2-54-233-120-17.sa-east-1.compute.amazonaws.com:8000/api/",
 
   timeout: 5000,
 });
