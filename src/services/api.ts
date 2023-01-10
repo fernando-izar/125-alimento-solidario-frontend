9cf2a124904e55631d5677ec323982970b51eb47
fernando-izar <fizar@me.com>
2023-01-10 08:54:32 -0300

    feat: Idonation interface changed

--- src/services/api.ts
+++ src/services/api.ts
@@ -5,10 +5,7 @@ const api = axios.create({
   // baseURL: "http://localhost:3000/",
   // baseURL: "http://localhost:8000/api/",
   baseURL: "http://ec2-54-233-120-17.sa-east-1.compute.amazonaws.com:8000/api/",
-  timeout: 5000,
-  headers: {
-    "Content-Security-Policy": "upgrade-insecure-requests",
-  },
+  timeout: 15000,
 });
 
 export default api;
